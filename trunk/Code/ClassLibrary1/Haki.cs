8618daa7d328d0045ebc0ca41b3d885900cd6f8e
flbuhot <flbuhot@2a56a29b-ecf5-42fa-ba4d-1dcc01156573>
2021-05-05 13:26:38 +0000

    Ajout classes + modification DiagrammeClasse.mdj

---
+++ trunk/Code/ClassLibrary1/Haki.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Haki
+    {
+        public Haki(string nom, string description)
+        {
+            Nom = nom;
+            Description = description;
+        }
+
+        private string nom;
+        public string Nom { get => nom; set => nom = value; }
+
+        private string description;
+        public string Description { get => description; set => description = value; }
+    }
+}
---
+++ trunk/Code/ClassLibrary1/Image.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Image
+    {
+        private string source;
+        private string description;
+
+        public Image(string source, string description)
+        {
+            Source = source;
+            Description = description;
+        }
+
+        public string Source { get => source; set => source = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
