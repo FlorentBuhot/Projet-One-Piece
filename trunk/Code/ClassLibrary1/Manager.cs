2a3556d7079202b64bda30a0c6aaae5d8d9de313
flbuhot <flbuhot@2a56a29b-ecf5-42fa-ba4d-1dcc01156573>
2021-05-07 07:54:35 +0000

    Ajout Manager.cs

---
+++ trunk/Code/ClassLibrary1/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Manager
+    {
+        private HashSet<Personnage> listePresos;
+        public void AjouterPerso(Personnage perso)
+        {
+            if (listePresos.Contains(perso))
+            {
+                return;
+            }
+            else
+            {
+                listePresos.Add(perso);
+            }
+        }
+
+        public void AjouterImgPerso(Image img, Personnage perso)
+        {
+            if (listePresos.Contains(perso))
+            {
+                
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
