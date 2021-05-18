b4f1bb5578e85aa7cff1cf13f05ebe05d06218fe
flbuhot <flbuhot@2a56a29b-ecf5-42fa-ba4d-1dcc01156573>
2021-05-18 10:42:36 +0000

    Ajout du test pour l'arc et le nomArc, solution pour le HashCode pour passé de l'arc précédent à l'arc suivant

---
+++ trunk/Code/Test_Arc+Nom_Arc/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace Test_Arc_Nom_Arc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arc alabasta = new Arc(NomArc.Alabasta, "Dans la prison", new Image("sgd", "gsgs"));
+
+            Console.WriteLine(NomArc.Alabasta.GetHashCode());
+            Console.WriteLine(alabasta.GetHashCode());
+        }
+    }
+}
