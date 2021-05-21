f7f02a79cfd5147be9371632161b2cc535e4ae89
flbuhot <flbuhot@2a56a29b-ecf5-42fa-ba4d-1dcc01156573>
2021-05-21 06:34:19 +0000

    Finalisation DiagrammeClasse.mdj + ajout UnitsTests

---
+++ Code/UnitsTests/UnitsTests_Arc.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitsTests
+{
+    public class UnitsTests_Arc
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
