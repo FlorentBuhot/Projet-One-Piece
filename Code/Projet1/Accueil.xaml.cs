2ba3368cf26d9f09fc59fec1e739a26554a1d28e
flbuhot <flbuhot@2a56a29b-ecf5-42fa-ba4d-1dcc01156573>
2021-05-25 12:49:55 +0000

    Mise en place des boutons à la place des polygone

--- Code/Projet1/Accueil.xaml.cs
+++ Code/Projet1/Accueil.xaml.cs
@@ -39,5 +39,130 @@ namespace Projet1
             (App.Current as App).MonManager.ArcAAfficher(1);
             (App.Current as App).Navigator.EtatEnCours = Navigator.EtatUC.ARC;
         }
+
+        private void Laboon(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Longue_Town(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_18(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_19(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_20(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_21(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_22(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
