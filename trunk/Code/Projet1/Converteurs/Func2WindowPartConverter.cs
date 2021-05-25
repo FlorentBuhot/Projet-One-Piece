121f24fed181d5cc9a44bb29f6381d7653f08ad4
khloichet <khloichet@2a56a29b-ecf5-42fa-ba4d-1dcc01156573>
2021-05-25 11:38:02 +0000

    Ajout de converteurs

---
+++ trunk/Code/Projet1/Converteurs/Func2WindowPartConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+using static Projet1.Navigator;
+
+namespace Projet1
+{
+    public class Func2WindowPartConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            EtatUC e = (EtatUC)value;
+
+            switch (e)
+            {
+                case Navigator.EtatUC.ACCUEIL:
+                    return new Accueil();
+                case Navigator.EtatUC.AJT_IMG_ARC:
+                    return new ajoutImageArc();
+                case Navigator.EtatUC.PERSONNAGE:
+                    return new DescriptionPerso();
+                case Navigator.EtatUC.ARC:
+                    return new DescriptionArc();
+                default:
+                    break;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
